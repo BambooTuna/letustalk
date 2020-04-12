e1d9983d1b3e7ac213bffc0ac57edcef90f1ca97
Suzuki Takeo <takeo@stko.info>
2020-04-13 02:36:02 +0900

    infra

---
+++ infra/rdb/mysql/1_table.sql
@@ -0,0 +1,13 @@
+DROP SCHEMA IF EXISTS letustalk;
+CREATE SCHEMA letustalk;
+USE letustalk;
+
+CREATE TABLE `invoice_detail` (
+    `invoice_id` VARCHAR(255) NOT NULL,
+    `amount` bigint(20) NOT NULL,
+    `paid` bit NOT NULL default false,
+    PRIMARY KEY (`invoice_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `invoice_detail` (`invoice_id`, `amount`) VALUES ("f0c28384-3aa4-3f87-9fba-66a0aa62c504", 5000);
+
+

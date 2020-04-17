ade79989fe97269bc6009ace73681345f7c3188a
Suzuki Takeo <takeo@stko.info>
2020-04-18 05:04:52 +0900

    AccountCredentialsRepository追加

--- backend/domain/repository/account.go
+++ backend/domain/repository/account.go
@@ -5,6 +5,13 @@ import (
 	"github.com/BambooTuna/letustalk/backend/domain"
 )
 
+type AccountCredentialsRepository interface {
+	Insert(record *domain.AccountCredentials) error
+	Update(record *domain.AccountCredentials) error
+	ResolveByMail(mail string) (*domain.AccountCredentials, error)
+	ResolveByAccountId(accountId string) (*domain.AccountCredentials, error)
+}
+
 type AccountDetailRepository interface {
 	//Insert(record *domain.AccountDetail) error
 	//Update(record *domain.AccountDetail) error

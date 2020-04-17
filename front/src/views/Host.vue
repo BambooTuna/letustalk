2d856eb91e6da121d58aca4fdc31476ab911a444
yutify <yu777developer@gmail.com>
2020-04-17 11:59:51 +0900

    add content page

---
+++ front/src/views/Host.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+    <h1>説明</h1>
+</div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+export default class Host extends Vue {
+}
+
+</script>
